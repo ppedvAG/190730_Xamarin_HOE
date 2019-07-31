cac30702919eef63081ca3f1cf046afe5670af69
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-31 13:25:56 +0200

    NavigationPage

--- Navigation_Demo/Navigation_Demo/Navigation_Demo/App.xaml.cs
+++ Navigation_Demo/Navigation_Demo/Navigation_Demo/App.xaml.cs
@@ -10,7 +10,8 @@ namespace Navigation_Demo
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            // MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
