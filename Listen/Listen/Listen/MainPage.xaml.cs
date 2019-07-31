3ddea51ebb3ab318587018b0718a9eb5c0c94b0f
Michael Zöhling <michael.zoehling@outlook.com>
2019-07-31 10:40:07 +0200

    Neuer Datentyp: Person

--- Listen/Listen/Listen/MainPage.xaml.cs
+++ Listen/Listen/Listen/MainPage.xaml.cs
@@ -20,7 +20,19 @@ namespace Listen
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            string[] personen = { "Tom Ate", "Anna Nass", "Peter Silie", "Martha Pfahl", "Franz Ose", "Klara Fall", "Rainer Zufall", "Frank N Stein", "Albert Tross", "Axel Schweiß", "Bill Dung", "Anna Bolika", "Clair Grube" };
+            Person[] personen =
+            {
+                new Person{Vorname="Tom",Nachname ="Ate",Alter=10,Kontostand = 100},
+                new Person{Vorname="Anna",Nachname ="Nass",Alter=20,Kontostand = 123},
+                new Person{Vorname="Peter",Nachname ="Silie",Alter=30,Kontostand = 1042340},
+                new Person{Vorname="Franz",Nachname ="Ose",Alter=40,Kontostand = -234234100},
+                new Person{Vorname="Martha",Nachname ="Pfahl",Alter=50,Kontostand = 9087654},
+                new Person{Vorname="Rainer",Nachname ="Zufall",Alter=60,Kontostand = 234234},
+                new Person{Vorname="Klara",Nachname ="Fall",Alter=70,Kontostand = 12113100},
+                new Person{Vorname="Albert",Nachname ="Tross",Alter=80,Kontostand = -123123100},
+                new Person{Vorname="Anna",Nachname ="Bolika",Alter=90,Kontostand = 4423423423},
+                new Person{Vorname="Clair",Nachname ="Grube",Alter=100,Kontostand = -12312312312312312},
+            };
             collectionViewPersonen.ItemsSource = personen;
         }
     }
