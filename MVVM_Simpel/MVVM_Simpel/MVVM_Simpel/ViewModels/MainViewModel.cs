f49e617931903aa2e8e15943df01415d74f3016c
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-01 09:23:44 +0200

    ViewModel erstellt und mit der View "verdrahtet"

---
+++ MVVM_Simpel/MVVM_Simpel/MVVM_Simpel/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MVVM_Simpel.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVM_Simpel.ViewModels
+{
+    class MainViewModel
+    {
+        public MainViewModel()
+        {
+            // Kontrollfreak-Antipattern
+            service = new PersonenService();
+
+            GetPersonenCommand = new Command(GetPersonen);
+        }
+
+        private readonly PersonenService service;
+
+        public List<Person> Personen { get; set; }
+
+        public ICommand GetPersonenCommand { get; set; }
+        private void GetPersonen(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
