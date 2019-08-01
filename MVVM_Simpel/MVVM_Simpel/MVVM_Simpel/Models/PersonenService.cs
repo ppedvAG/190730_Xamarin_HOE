63949a6f52ad3226ab7a1ce4534d9e9f232ce4ac
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-01 08:56:43 +0200

    Model erstellt

---
+++ MVVM_Simpel/MVVM_Simpel/MVVM_Simpel/Models/PersonenService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVM_Simpel.Models
+{
+    class PersonenService
+    {
+        public List<Person> GetPersonen()
+        {
+            return new List<Person>
+            {
+                new Person{Vorname="Tom",Nachname ="Ate",Alter=10,Kontostand = 100},
+                new Person{Vorname="Anna",Nachname ="Nass",Alter=20,Kontostand = 123},
+                new Person{Vorname="Peter",Nachname ="Silie",Alter=30,Kontostand = 1042340},
+                new Person{Vorname="Franz",Nachname ="Ose",Alter=40,Kontostand = -234234100},
+                new Person{Vorname="Martha",Nachname ="Pfahl",Alter=50,Kontostand = 9087654},
+                new Person{Vorname="Rainer",Nachname ="Zufall",Alter=60,Kontostand = 234234},
+                new Person{Vorname="Klara",Nachname ="Fall",Alter=70,Kontostand = 12113100},
+                new Person{Vorname="Albert",Nachname ="Tross",Alter=80,Kontostand = -123123100},
+                new Person{Vorname="Anna",Nachname ="Bolika",Alter=90,Kontostand = 4423423423},
+                new Person{Vorname="Clair",Nachname ="Grube",Alter=100,Kontostand = -12312312312312312},
+            };
+        }
+    }
+}
