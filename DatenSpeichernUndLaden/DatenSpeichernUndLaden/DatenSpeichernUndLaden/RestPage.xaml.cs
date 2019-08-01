37247adcf7e5f774415fcdf3a59cde287a558c2e
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-01 13:50:00 +0200

    REST-Service abgreifen und die Daten in einer ListView anzeigen

---
+++ DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden/RestPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DatenSpeichernUndLaden
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RestPage : ContentPage
+    {
+        public RestPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ListView_Refreshing(object sender, EventArgs e)
+        {
+            // 1) Herunterladen
+            string json;
+            using(HttpClient client = new HttpClient())
+            {
+                json = await client.GetStringAsync("https://jsonplaceholder.typicode.com/todos");
+            }
+
+            // 2) Deserialisieren und Anzeigen
+            listViewToDo.ItemsSource = JsonConvert.DeserializeObject<ToDoItem[]>(json);
+
+            listViewToDo.EndRefresh();
+        }
+    }
+}
---
+++ DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden/ToDoItem.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatenSpeichernUndLaden
+{
+    class ToDoItem
+    {
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("completed")]
+        public bool Completed { get; set; }
+    }
+
+}
