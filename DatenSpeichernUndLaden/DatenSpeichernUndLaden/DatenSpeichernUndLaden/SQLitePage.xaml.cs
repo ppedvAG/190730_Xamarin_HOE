1fbcf03d689a8198404156e52d99d2878c831c85
Michael Zöhling <michael.zoehling@outlook.com>
2019-08-01 14:35:25 +0200

    SQLite

---
+++ DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden/SQLitePage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DatenSpeichernUndLaden
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SQLitePage : ContentPage
+    {
+        public SQLitePage()
+        {
+            InitializeComponent();
+
+            string databasePath = Path.Combine(FileSystem.AppDataDirectory, "demo.db");
+            con = new SQLiteConnection(databasePath); // Wenn die DB nicht existiert, wird sie automatisch erstellt
+            con.CreateTable<ToDoItem>(); // Wenn die Tabelle bereits existiert, passiert nichts 
+        }
+
+        private SQLiteConnection con;
+
+        private void ListView_Refreshing(object sender, EventArgs e)
+        {
+            listViewToDo.ItemsSource = con.Table<ToDoItem>().ToList();
+            listViewToDo.EndRefresh();
+            con.
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ToDoItem item = new ToDoItem { Title = entryTitle.Text, Completed = switchCompleted.IsToggled };
+            con.Insert(item);
+        }
+    }
+}
--- DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden/ToDoItem.cs
+++ DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden/ToDoItem.cs
@@ -2,15 +2,23 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
 
 namespace DatenSpeichernUndLaden
 {
     class ToDoItem
     {
+
         [JsonProperty("userId")]
         public int UserId { get; set; }
+
+        [PrimaryKey,AutoIncrement]
+
         [JsonProperty("id")]
         public int Id { get; set; }
+
+        [MaxLength(255)]
+
         [JsonProperty("title")]
         public string Title { get; set; }
         [JsonProperty("completed")]
